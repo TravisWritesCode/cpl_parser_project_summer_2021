e2f4e4732cfe7514257688fc48e21100a9728750
Nicholas <Lenweve@gmail.com>
2021-06-18 18:15:20 -0400

    Completed Lexer.getNextToken

--- src/main/java/cpl_parser_project_summer_2021/parser/Lexer.java
+++ src/main/java/cpl_parser_project_summer_2021/parser/Lexer.java
@@ -2,10 +2,41 @@ package cpl_parser_project_summer_2021.parser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 import static cpl_parser_project_summer_2021.parser.Token.Type.*;
+import static java.lang.Character.toUpperCase;
 
 public class Lexer {
+	
+	private static Map<String, Token> keywords = Map.ofEntries(
+		keyword(CLOSE),
+		keyword(DATA),
+		keyword(DIM),
+		keyword(END),
+		keyword(FOR),
+		keyword(GOTO),
+		keyword(GOSUB),
+		keyword(IF),
+		keyword(INPUT),
+		keyword(LET),
+		keyword(NEXT),
+		keyword(OPEN),
+		keyword(POKE),
+		keyword(PRINT),
+		keyword(READ),
+		keyword(RETURN),
+		keyword(RESTORE),
+		keyword(RUN),
+		keyword(STOP),
+		keyword(SYS),
+		keyword(WAIT),
+		keyword(OUTPUT)
+	);
+	
+	private static Map.Entry<String, Token> keyword(Token.Type type) {
+		return Map.entry(type.name(), new Token(type.name(), type));
+	}
 	
 	private InputStream input;
 	private char current;
@@ -29,22 +60,30 @@ public class Lexer {
 		} else if ((current >= 'A' && current <= 'Z') || (current >= 'a' && current <= 'z')) {
 			var builder = new StringBuilder();
 			while (!eof && (current >= 'A' && current <= 'Z' || current >= 'a' && current <= 'z')) {
-				builder.append(current);
+				builder.append(toUpperCase(current));
 				nextChar();
 			}
 			if (!eof && (current == '$' || current == '%')) {
 				builder.append(current);
 				nextChar();
 			}
-			// TODO: check for keyword here
-			return new Token(builder.toString(), ID);
+			var value = builder.toString();
+			var keyword = keywords.get(value);
+			if (keyword != null) {
+				return keyword;
+			} else if (REM.name().equals(value)) {
+				while (!eof && current != '\r' && current != '\n') nextChar();
+				return new Token(null, REM);
+			} else {
+				return new Token(value, ID);
+			}
 		} else if (current == '"') {
 			var builder = new StringBuilder();
 			nextChar(); // current is '"'
 			while (current != '"') {
 				builder.append(current);
 				nextChar();
-				if (eof) throw new LexerException("Unterminated String Literal");
+				if (eof || current == '\r' || current == '\n') throw new LexerException("Unterminated String Literal");
 			}
 			nextChar(); // current is '"'
 			return new Token(builder.toString(), STRING);
@@ -57,8 +96,54 @@ public class Lexer {
 				nextChar();
 			}
 			return new Token(builder.toString(), decimal ? REAL : INTEGER);
+		} else if (current == '\r' || current == '\n') {
+			var first = current;
+			nextChar();
+			if (!eof && first == '\r' && current == '\n') {
+				nextChar();
+				return new Token("\r\n", NEWLINE);
+			} else {
+				return new Token(String.valueOf(first), NEWLINE);
+			}
 		} else {
-			throw new LexerException("Unexpected character '" + current + "'");
+			var advance = true;
+			var token = switch (current) {
+				case ':' -> new Token(":", COLON);
+				case '#' -> new Token("#", HASH);
+				case '(' -> new Token("(", LPAREN);
+				case ')' -> new Token(")", RPAREN);
+				case ',' -> new Token(",", COMMA);
+				case ';' -> new Token(";", SEMI);
+				case '=' -> new Token("=", EQUALS);
+				case '<' -> {
+					nextChar();
+					if (!eof) {
+						if (current == '=') {
+							yield new Token("<=", LTE);
+						} else if (current == '>') {
+							yield new Token("<>", DIAMOND);
+						}
+					}
+					advance = false;
+					yield new Token("<", LT);
+				}
+				case '>' -> {
+					nextChar();
+					if (!eof && current == '=') {
+						yield new Token(">=", GTE);
+					}
+					advance = false;
+					yield new Token(">", GT);
+				}
+				case '+' -> new Token("+", PLUS);
+				case '-' -> new Token("-", MINUS);
+				case '*' -> new Token("*", TIMES);
+				case '/' -> new Token("/", DIVIDE);
+				case '^' -> new Token("^", POWER);
+				default -> throw new LexerException("Unexpected character '" + current + "'");
+			};
+			if (advance) nextChar();
+			return token;
 		}
 	}
 		
