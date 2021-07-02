227629c7a38d3c92f1a0d7c3d067d4b1e612d68a
TravisWritesCode <37280847+TravisWritesCode@users.noreply.github.com>
2021-07-02 18:57:21 -0400

    keywords.txt added to get 2 pts back

--- src/main/java/cpl_parser_project_summer_2021/parser/Token.java
+++ src/main/java/cpl_parser_project_summer_2021/parser/Token.java
@@ -1,3 +1,12 @@
+/*
+ * Class:       CS 4308 Section 1
+ * Term:        Fall 2019
+ * Name:        Nicholas Luchuk, Travis Hescox, Yash Shidhaye,
+ * Instructor:   Deepa Muralidhar
+ * Project:  Deliverable 1 Scanner - Java
+ */
+
+
 package cpl_parser_project_summer_2021.parser;
 
 public record Token(String lexeme, Type type) {
